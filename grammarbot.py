fa20892e529919187fb55b18d6253ee29c2d3a1d
Trent Nielsen <trentnielsen84@gmail.com>
2020-03-23 10:39:59 -0400

    test ability to enable/disable spell check

--- grammarbot.py
+++ grammarbot.py
@@ -37,54 +37,75 @@ def serialize_list(list):
 # Note: Paid features provide better accuracy, but for someone having problems with their, there and there, this is
 # a good choice.
 def check_grammar(**payload):
+    global grammarck_user
+    global grammarck_user_id
+    global grammarck_status
     data = payload['data']
+    web_client = payload['web_client']
+    channel_id = data['channel']
+    user_id = data['user']
+    channel_id = data['channel']
+    text = data['text']
     if 'user' in data:
-        # Set some function variables
-        web_client = payload['web_client']
-        user_id = data['user']
-        channel_id = data['channel']
-        text = data['text']
-        response = requests.post('http://api.grammarbot.io/v2/check', data={
-            'api_key': grammarbot_token,
-            'language': 'en-US',
-            'text': text
-        })
-        if response.status_code == 200:
-            dict = response.json()
-            matches = dict['matches']
-            if debug == 'true':
-                print('DEBUG-Grammarbot-check_grammar: matches variable type:{}'.format(type(matches)))
-                print('DEBUG-Grammarbot-check_grammar: matches variable value:{}'.format(matches))
-            if matches != []:
-                messages.send_channel_message('You have {} grammar mistakes in the following text:'.format(str(len(matches))), channel_id)
-                messages.send_channel_message('>{}'.format(text), channel_id)
-                x = 0
-                for error in matches:
-                    x = x + 1
-                    messages.send_channel_message('********** ERROR NUMBER {} **********'.format(str(x)), channel_id)
-                    messages.send_channel_message('Found in the following sentence:', channel_id)
-                    messages.send_channel_message('>{}'.format(error['sentence']), channel_id)
-                    messages.send_channel_message('Error rule: {}'.format(error['rule']['description']), channel_id)
-                    replacements = []
-                    if error['shortMessage'] == 'Spelling mistake':
-                        for replacement in error['replacements']:
-                            replacements.append(replacement['value'])
-                        messages.send_channel_message('Suggestion: Learn to spell, or pay careful attention to the red line under what you type. '
-                              'Did you mean `{}`?'.format(serialize_list(replacements)), channel_id)
-                    else:
-                        messages.send_channel_message('Suggestion: {}'.format(error['message']), channel_id)
-                    print('')
-                x = 0
-                return response
+        # Allow meme council to disable grammarbot. If disabled, reset the status and users
+        if ('!disable grammarbot' in data['text']) and (data['user'] in users.meme_council_ids.values()):
+            grammarck_status = False
+            web_client.chat_postMessage(
+                channel=channel_id,
+                text=f"<@{grammarck_user}> is no longer being grammar checked."
+            )
+            grammarck_user = 'none'
+            grammarck_user_id = 'none'
+        # Allow meme council to enable grammarbot.
+        elif ('!enable grammarbot' in data['text']) and (data['user'] in users.meme_council_ids.values()):
+            grammarck_status = True
+            grammarck_user = 'tnielsen'
+            grammarck_user_id = messages.get_user_id(grammarck_user)
+            web_client.chat_postMessage(
+                channel=channel_id,
+                text=f"<@{grammarck_user}> <@{grammarck_user}> is now being grammar checked in every channel we share."
+            )
         else:
-            data = {
-                'Error': 'Not 200'
-            }
-            return JsonResponse(data)
+            # Turn on grammarcheck is on and the user is in the channel, then grammar check them. 
+            if (grammarck_status is True) and (messages.in_channel(grammarck_user, channel_id)):
+                # Set some function variables
+                # Create object from grammarbot api
+                response = requests.post('http://api.grammarbot.io/v2/check', data={
+                    'api_key': grammarbot_token,
+                    'language': 'en-US',
+                    'text': text
+                })
+                if response.status_code == 200:
+                    dict = response.json()
+                    matches = dict['matches']
+                    if debug == 'true':
+                        print('DEBUG-Grammarbot-check_grammar: matches variable type:{}'.format(type(matches)))
+                        print('DEBUG-Grammarbot-check_grammar: matches variable value:{}'.format(matches))
+                    if matches != []:
+                        messages.send_channel_message('You have {} grammar mistakes in the following text:'.format(str(len(matches))), channel_id)
+                        messages.send_channel_message('>{}'.format(text), channel_id)
+                        x = 0
+                        for error in matches:
+                            x = x + 1
+                            messages.send_channel_message('********** ERROR NUMBER {} **********'.format(str(x)), channel_id)
+                            messages.send_channel_message('Found in the following sentence:', channel_id)
+                            messages.send_channel_message('>{}'.format(error['sentence']), channel_id)
+                            messages.send_channel_message('Error rule: {}'.format(error['rule']['description']), channel_id)
+                            replacements = []
+                            if error['shortMessage'] == 'Spelling mistake':
+                                for replacement in error['replacements']:
+                                    replacements.append(replacement['value'])
+                                messages.send_channel_message('Suggestion: Learn to spell, or pay careful attention to the red line under what you type. '
+                                      'Did you mean `{}`?'.format(serialize_list(replacements)), channel_id)
+                            else:
+                                messages.send_channel_message('Suggestion: {}'.format(error['message']), channel_id)
+                            print('')
+                        x = 0
+                        return response
+                else:
+                    data = {
+                        'Error': 'Not 200'
+                    }
+                    return JsonResponse(data)
     else:
         return None
-
-# debugging output for testing
-#test = check_grammar('How do I Get their? This is SIlY')
-# print(json.dumps(test.json(), indent=2))
-
